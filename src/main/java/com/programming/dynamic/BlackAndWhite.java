1fa3b6293905255837e76bd61a201514a12ef39c
Nik Nenkov <nn8543@gmail.com>
2020-05-25 22:22:31 +0300

    JDK11 var instead of lombok.val

--- src/main/java/com/programming/dynamic/BlackAndWhite.java
+++ src/main/java/com/programming/dynamic/BlackAndWhite.java
@@ -1,22 +1,20 @@
 package com.programming.dynamic;
 
 import lombok.extern.java.Log;
-import lombok.val;
 
 import java.util.Scanner;
 
 @Log
 public class BlackAndWhite {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        var scanner = new Scanner(System.in);
 
-        final int sideA = sc.nextInt();
-        final int sideB = sc.nextInt();
+        var sideA = scanner.nextInt();
+        var sideB = scanner.nextInt();
 
-        val solver = new BlackAndWhiteSolver(sideA,sideB);
+        var solver = new BlackAndWhiteSolver(sideA, sideB);
 
         solver.findNumberOfPossibleSolutions();
-
         log.info(solver.getNumberOfSolutions().toString());
     }
 }
